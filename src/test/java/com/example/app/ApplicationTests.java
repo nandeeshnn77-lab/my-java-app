efc65eca9b2390b28740e0b2ddf00ac6282c786f
nandeeshnn77-lab <nandeeshnn77@gmail.com>
2026-08-12 12:47:46 +0530

    Add initial unit test for application

---
+++ src/test/java/com/example/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTests {
+
+    @Test
+    void applicationTest() {
+        assertTrue(true);
+    }
+}
